4ac34385d13576a6e24fce86b1420a10153f4d91
Simz014 <asimaha123@gmail.com>
2022-01-10 13:59:25 -0600

    added while loop to verify user input

--- Payroll.scala
+++ Payroll.scala
@@ -1,6 +1,8 @@
 import java.util.Scanner
 import java.sql.DriverManager
 import java.sql.Connection
+import java.io._
+import javax.crypto.BadPaddingException
 
 object Payroll {
 
@@ -28,51 +30,89 @@ object Payroll {
    
       // create the statement, and run the select query
      val statement = connection.createStatement()
-      /*This create the Employee table for Payroll Database 
-      val creat_Table = statement.executeUpdate("Create table Employee(EmployeeID serial, fName varchar(255),lName varchar(255), Employment_Type varchar(255),  "+
-                                              " hoursWorked Decimal (10,2), Hourly_Rate Decimal(10,2),Date_Time datetime default current_timestamp on Update current_timestamp);") 
-                                              */
+     /*This is ran once to create the Employee table for Payroll Database 
+     val creat_Table = statement.executeUpdate("Create table Employee(EmployeeID serial, firstName varchar(255),lastName varchar(255), Employment_Type varchar(255),  "+
+                                             " hoursWorked Decimal (10,2), Hourly_Rate Decimal(10,2),Date_Time datetime default current_timestamp on Update current_timestamp);") 
+                                            */
+      /*This is ran once to create the view in database*/  
+    /** val creat_view = statement.executeUpdate("create view  Paycheck AS select EmployeeID, concat(firstName ,' ', lastName) as FullName, Employment_Type,"+
+                                               " Hourly_Rate,hoursWorked,(hoursWorked-40) as Overtime, (Hourly_Rate*hoursWorked ) as TotalPay from Employee where EmployeeID =(SELECT max(EmployeeID) FROM Employee);") 
+                                                                                                     
+                                   **/
+        
+            println("Please eneter your First Name(A-Z)")
+               var firstName=scanner.nextLine() 
+        
+           println("Please eneter your Last Name(A-Z)")
+          var  lastName=scanner.nextLine()
+            
+            println("Please eneter your total Weekly hours")   
+            var  hoursWorked= scanner.nextInt()
+            
+          if(hoursWorked<=40) {
+           paymentAmount=hoursWorked*rate
+              
+          }
        
-          println("Please eneter your LName(A-z)")
-          var fName=scanner.nextLine()                                      
-         println("Please eneter your LName(A-z)")
-         var lName=scanner.nextLine() 
-         println("Please eneter your total Weekly hours")   
-         var hoursWorked= scanner.nextInt()
-          scanner.nextLine()
-         println("Please enter S for Salaried and H for Hourly") 
-         var typeEmployment = scanner.nextLine()
-         if((typeEmployment=="S")&&(typeEmployment=="s")){
-            typeEmployment="Salaried"
-         } else if((typeEmployment=="h") && (hoursWorked<=40)) {
-              paymentAmount=hoursWorked*rate
-              typeEmployment="hourly"
-               
-         }else{
-           paymentAmount=(40*rate + (hoursWorked-40)*rate*1.5)
-         }  
-         val query="INSERT INTO employee(fName, lName,Employment_Type, hoursWorked,Hourly_Rate) Values ('"+fName+"','"+lName+"','"+typeEmployment+"'," +
-                                                         ""+hoursWorked+","+rate+");"
-        //println(query)
-             val resultSet=statement.executeUpdate(query)
+          while (hoursWorked>=60){
+            println("Company does not allow working more than 60hrs be week")
+            println("Please eneter your total Weekly hours") 
+            hoursWorked= scanner.nextInt()
+            if((hoursWorked < 60) && (hoursWorked > 0) )
+             hoursWorked;
+          }
+          
+          
+          /** Overtime calculation **/
+          paymentAmount=(40*rate + (hoursWorked-40)*rate*1.5)
+          
+          
+         println(" Is this info correct? enter Y/N")
+         println("FullName :" +firstName+" "+lastName+ " :Hours Worked:" +hoursWorked)
+         println("========================================================================================================")
+          var verification=scanner.next().toString.toUpperCase()
+          val query="INSERT INTO employee(firstName, lastName,Employment_Type, hoursWorked,Hourly_Rate) Values ('"+firstName+"','" +lastName+"','"+typeEmployment+"'," +
+                                                     ""+hoursWorked+","+rate+");" 
+       // println(query)
+        val resultSet1=statement.executeUpdate(query)
 
-       val creat_view = statement.executeUpdate("create view  Paycheck AS select EmployeeID, concat(fname , lname) as FullName, Employment_Type,"+
-                                                 "Hourly_Rate,hoursWorked,(hoursWorked-40) as Overtime, (Hourly_Rate*hoursWorked ) as TotalPay from Employee;" ) 
-                                                                                     
-       
+          while (verification =="N"){
+            val resultSet2=statement.executeUpdate("delete from employee order by EmployeeID desc limit 1;")
+               println("Please try again")
+               println("Please eneter your First Name(A-Z)")
+               var firstName=scanner.next() 
+             println("Please eneter your Last Name(A-Z)")
+            var  lastName=scanner.next()
+            
+            println("Please eneter your total Weekly hours")   
+            var  hoursWorked= scanner.nextInt()   
+            println(" Is this info correct? enter Y/N")
+          println("FullName :" +firstName+" "+lastName+ " :Hours Worked:" +hoursWorked)
+           println("========================================================================================================")
+          val query="INSERT INTO employee(firstName, lastName,Employment_Type, hoursWorked,Hourly_Rate) Values ('"+firstName+"','" +lastName+"','"+typeEmployment+"'," +
+                                                     ""+hoursWorked+","+rate+");" 
+
+            val resultSet1=statement.executeUpdate(query)
+               verification=scanner.next().toString.toUpperCase() 
+        }
+         /** Printing the headings**/
+       println("=============================================================================================================================================")                    
+       println("Employee_ID "+ "\t" +"FullName"+ "\t "  +"EmployeeType "+ "\t\t"  +"Hourly_Rate "+ "\t\t"  +"HoursWorked "+ "\t\t" +"Overtime "+ "\t\t"   +"TotalPay ")
+       println("=============================================================================================================================================")
         //val resultDb = statement.executeQuery("SELECT * FROM employee;")
-        val resultDbView = statement.executeQuery("SELECT * FROM Paycheck;")                                                                        
+        val resultDbView = statement.executeQuery("SELECT * FROM Paycheck;")
+                                                                                 
       while ( resultDbView.next())
-        print(resultDbView.getString(1) + " " + resultDbView.getString(2) + " " + resultDbView.getString(3))
+        
+        println(resultDbView.getString(1) + "\t\t " + resultDbView.getString(2) + " \t\t  " + resultDbView.getString(3)+ " \t\t  "+resultDbView.getString(4)
+        + " \t\t  "+resultDbView.getString(5)+ " \t\t  "+resultDbView.getString(6)+ " \t\t\t "+resultDbView.getString(7)) 
         println()
       }
-
-     catch {
+    
+    catch {
       case e: Exception => e.printStackTrace
     }
     connection.close()
-    }
-  
-    
+}
 
 }
